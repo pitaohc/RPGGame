f226575b82a0a8a6f6b5a585af67e27cee98a40e
pitao <pitaohc@qq.com>
2025-12-02 18:11:34 +0800

    50. Endless Background

--- Assets/src/ParallalLayer.cs
+++ Assets/src/ParallalLayer.cs
@@ -5,9 +5,34 @@ public class ParallalLayer
 {
     [SerializeField] private Transform layerTransform;
     [SerializeField] private float parallaxFactor;
+    [SerializeField] private float safetyWidth = 0.0f;
+
+    private float imageFullWidth;
+    private float imageHalfWidth;
 
     public void Move(float deltaX)
     {
         layerTransform.position += Vector3.right * (deltaX * parallaxFactor);
+    }
+
+    public void LoopBackground(float cameraLeftEdge, float cameraRightEdge)
+    {
+        float imageLeftEdge = layerTransform.position.x - imageHalfWidth;
+        float imageRightEdge = layerTransform.position.x + imageHalfWidth;
+        if (imageRightEdge < cameraLeftEdge + safetyWidth)
+        {
+            layerTransform.position += Vector3.right * imageFullWidth;
+        }
+        else if (imageLeftEdge > cameraRightEdge - safetyWidth)
+        {
+            layerTransform.position += Vector3.left * imageFullWidth;
+        }
+    }
+
+    public void CalculateImageWidth()
+    {
+        imageFullWidth = layerTransform.GetComponent<SpriteRenderer>().bounds.size.x;
+        imageHalfWidth = imageFullWidth / 2f;
+        //Debug.Log($"Image Width for {layerTransform.name}: {imageFullWidth}");
     }
 }
--- Assets/src/ParallaxBackground.cs
+++ Assets/src/ParallaxBackground.cs
@@ -7,23 +7,42 @@ public class ParallaxBackground : MonoBehaviour
 
     private Camera mainCamera;
     private float lastCameraPositionX;
+    private float cameraHalfWidth;
+    private float cameraFullWidth;
 
     private void Awake()
     {
         mainCamera = Camera.main;
         lastCameraPositionX = mainCamera.transform.position.x;
+
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        cameraFullWidth = cameraHalfWidth * 2f;
+
+        CalculateImageWidth();
     }
 
     // Move is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
+        //Debug.Log($"FC: {Time.frameCount}, dT: {Time.deltaTime}");
         float cameraPositionX = mainCamera.transform.position.x;
         float deltaX = cameraPositionX - lastCameraPositionX;
+        //Debug.Log($"{Time.frameCount} Delta X: {deltaX}， cur: {cameraPositionX}, last: {lastCameraPositionX}");
         lastCameraPositionX = cameraPositionX;
-
+        float cameraLeftEdge = cameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = cameraPositionX + cameraHalfWidth;
         foreach (var layer in parallaxLayers)
         {
             layer.Move(deltaX);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+        }
+    }
+
+    private void CalculateImageWidth()
+    {
+        foreach (var layer in parallaxLayers)
+        {
+            layer.CalculateImageWidth();
         }
     }
 }
