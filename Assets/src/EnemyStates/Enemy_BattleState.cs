618a21bc8ba414e40222a18c2f576f63d474da5b
pitao <pitaohc@qq.com>
2025-12-24 00:04:41 +0800

    Resolve Dev-58: Chase & Attack

--- Assets/src/EnemyStates/Enemy_BattleState.cs
+++ Assets/src/EnemyStates/Enemy_BattleState.cs
@@ -2,14 +2,57 @@ using UnityEngine;
 
 public class Enemy_BattleState : EnemyState
 {
+    private Transform transform_player;
+
     public Enemy_BattleState(Enemy enemy, StateMachine stateMachine, string animBoolName) : base(enemy, stateMachine, animBoolName)
     {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        if (transform_player == null)
+        {
+            transform_player = enemy.PlayerDetection().transform;
+            //Debug.Log("set transform" + transform_player.ToString());
+        }
     }
 
     public override void Update()
     {
         base.Update();
 
+        if (WithAttackRange())
+        {
+            //Debug.Log("change state to attack");
+            stateMachine.ChangeState(enemy.attackState);
+        }
+        else
+        {
+            //Debug.Log("move to player");
+            enemy.SetVelocity(enemy.battleMoveVelocity * DirectionToPlayer(), rb.linearVelocityY);
+        }
+    }
+    private bool WithAttackRange() =>
+        DistanceToPlayer() <= enemy.attackDistance;
 
+    private int DirectionToPlayer()
+    {
+        if (transform_player == null)
+        {
+            return 0;
+        }
+
+        return (enemy.transform.position.x - transform_player.position.x > 0) ? -1 : 1;
+    }
+    private float DistanceToPlayer()
+    {
+        if (transform_player == null)
+        {
+            return float.MaxValue;
+        }
+
+        return Mathf.Abs(enemy.transform.position.x - transform_player.position.x);
+        //return Vector2.Distance(enemy.transform.position, transform_player.position);
     }
 }
--- Assets/src/StateMachine/EnemyState.cs
+++ Assets/src/StateMachine/EnemyState.cs
@@ -3,20 +3,26 @@ using UnityEngine;
 public class EnemyState : EntityState
 {
     protected Enemy enemy;
+    private int Id_xVelocity;
     public EnemyState(Enemy enemy, StateMachine stateMachine, string animBoolName) : base(stateMachine, animBoolName)
     {
         this.enemy = enemy;
 
         rb = enemy.rb;
         anim = enemy.anim;
+        Id_xVelocity = Animator.StringToHash("xVelocity");
+
     }
 
     public override void Update()
     {
         base.Update();
+
         if (Input.GetKeyDown(KeyCode.F))
         {
             stateMachine.ChangeState(enemy.attackState);
         }
+        anim.SetFloat(Id_xVelocity, rb.linearVelocityX);
+
     }
 }
