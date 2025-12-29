81579d45be395dee13592120cc78e61048656d03
pitao <pitaohc@qq.com>
2025-12-29 08:58:38 +0800

    完成击退，剩余重击需要制作

--- Assets/src/EntityHealth.cs
+++ Assets/src/EntityHealth.cs
@@ -1,23 +1,35 @@
 using System;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class EntityHealth : MonoBehaviour
 {
     private EntityVFX entityVFX;
+    private Entity entity;
     [SerializeField] protected float maxHealth = 100f;
     [SerializeField] protected bool isDead;
 
+    [Header("On Damage Knockback")]
+    [SerializeField] protected Vector2 knockbackPower = new(1.5f, 2.5f);
+    [SerializeField] protected float knockbackDuration = 0.2f;
+
+    //[Header("On Heavy Damage")]
+    //[SerializeField] protected float heavyDamageThreshold = .3f;
+    //[SerializeField] protected float heavyKnockbackDuration = .5f;
+    //[SerializeField] protected Vector2 heavyKnockbackPower = new(7, 7);
     protected virtual void Awake()
     {
         entityVFX = GetComponent<EntityVFX>();
+        entity = GetComponent<Entity>();
     }
 
     public virtual void TakeDamage(float damage, Transform damageDealer)
     {
-        if (isDead)
-        {
-            return;
-        }
+        if (isDead) return;
+
+        Vector2 knockback = CalculateKnockback(damageDealer);
+        entity?.ReceiveKnockback(knockback, knockbackDuration);
         entityVFX?.PlayOnDamageVfx();
         ReduceHealth(damage);
     }
@@ -36,4 +48,10 @@ public class EntityHealth : MonoBehaviour
         isDead = true;
         Debug.Log("Entity dead");
     }
+
+    //private Vector2 CalculateKnockback(float damage, Transform damageDealer)
+    //{
+    //    int direction = transform.position.x > damageDealer.position.x ? 1 : -1;
+    //    return knockbackPower * new Vector2(direction, 1);
+    //}
 }
