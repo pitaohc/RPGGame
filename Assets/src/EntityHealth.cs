f75ffb4276bd8501011e2d644e81f579dc201c36
pitao <pitaohc@qq.com>
2025-12-29 23:21:38 +0800

    Resolve Dev-65: On Damage Knockback 完成重击 Close #17

--- Assets/src/EntityHealth.cs
+++ Assets/src/EntityHealth.cs
@@ -7,19 +7,21 @@ public class EntityHealth : MonoBehaviour
 {
     private EntityVFX entityVFX;
     private Entity entity;
+    protected float curHealth = 100f;
     [SerializeField] protected float maxHealth = 100f;
     [SerializeField] protected bool isDead;
 
     [Header("On Damage Knockback")]
-    [SerializeField] protected Vector2 knockbackPower = new(1.5f, 2.5f);
     [SerializeField] protected float knockbackDuration = 0.2f;
+    [SerializeField] protected Vector2 knockbackPower = new(1.5f, 2.5f);
 
-    //[Header("On Heavy Damage")]
-    //[SerializeField] protected float heavyDamageThreshold = .3f;
-    //[SerializeField] protected float heavyKnockbackDuration = .5f;
-    //[SerializeField] protected Vector2 heavyKnockbackPower = new(7, 7);
+    [Header("On Heavy Damage")]
+    [SerializeField] protected float heavyDamageThreshold = .3f;
+    [SerializeField] protected float heavyKnockbackDuration = .5f;
+    [SerializeField] protected Vector2 heavyKnockbackPower = new(7, 7);
     protected virtual void Awake()
     {
+        curHealth = maxHealth;
         entityVFX = GetComponent<EntityVFX>();
         entity = GetComponent<Entity>();
     }
@@ -28,16 +30,17 @@ public class EntityHealth : MonoBehaviour
     {
         if (isDead) return;
 
-        Vector2 knockback = CalculateKnockback(damageDealer);
-        entity?.ReceiveKnockback(knockback, knockbackDuration);
+        Vector2 knockback = CalculateKnockback(damage, damageDealer);
+        float duration = CalculateDuration(damage);
+        entity?.ReceiveKnockback(knockback, duration);
         entityVFX?.PlayOnDamageVfx();
         ReduceHealth(damage);
     }
 
     protected void ReduceHealth(float damage)
     {
-        maxHealth -= damage;
-        if (maxHealth <= 0)
+        curHealth -= damage;
+        if (curHealth <= 0)
         {
             Die();
         }
@@ -49,9 +52,15 @@ public class EntityHealth : MonoBehaviour
         Debug.Log("Entity dead");
     }
 
-    //private Vector2 CalculateKnockback(float damage, Transform damageDealer)
-    //{
-    //    int direction = transform.position.x > damageDealer.position.x ? 1 : -1;
-    //    return knockbackPower * new Vector2(direction, 1);
-    //}
+    private Vector2 CalculateKnockback(float damage, Transform damageDealer)
+    {
+        int direction = transform.position.x > damageDealer.position.x ? 1 : -1;
+        Vector2 knockback = (damage < maxHealth * heavyDamageThreshold) ? knockbackPower : heavyKnockbackPower;
+        return knockback * new Vector2(direction, 1);
+    }
+
+    private float CalculateDuration(float damage)
+    {
+        return (damage < maxHealth * heavyDamageThreshold) ? knockbackDuration : heavyKnockbackDuration;
+    }
 }
