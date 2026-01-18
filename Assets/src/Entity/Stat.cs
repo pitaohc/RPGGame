a5b43a16af39b059808bc555c3427e0079f68965
pitao <pitaohc@qq.com>
2026-01-18 17:21:02 +0800

    Resolve Stat System: Stat Class 1. 创建Stat类存放基础数据 2. 创建EntityStat类聚合数据 3. 替换掉原先的maxHp
    Close #33

---
+++ Assets/src/Entity/Stat.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Stat
+{
+    [SerializeField] private float baseValue;
+    
+    public float GetValue()
+    {
+        return baseValue;
+    }
+}
