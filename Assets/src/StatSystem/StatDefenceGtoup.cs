c6b8262d05c14edc852bb1be315b922dfb0aae11
pitao <pitaohc@qq.com>
2026-01-18 17:58:29 +0800

    Resolve Stat System: Stat Groups
    1. 移动Stat
    2. 创建StatGroup
    
    Close #34

---
+++ Assets/src/StatSystem/StatDefenceGtoup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+
+[Serializable]
+public class StatDefenceGtoup
+{
+    public Stat armor; // 护甲
+    public Stat evasion; // 闪避
+    public Stat fireRes; // 火焰抗性
+    public Stat iceRes; // 冰冻抗性
+    public Stat lightingRes; // 光明抗性
+}
---
+++ Assets/src/StatSystem/StatMajorGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+
+[Serializable]
+public class StatMajorGroup
+{
+    public Stat strength; // 力量
+    public Stat agility; // 敏捷
+    public Stat intelligence; // 智力
+    public Stat vitality; // 体力
+}
