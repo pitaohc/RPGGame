3bca20e41702c737e3ef8afca40fd09c1f0d1531
pitaohc <36119151+pitaohc@users.noreply.github.com>
2026-01-18 17:59:41 +0800

    Resolve Stat System: Stat Groups (#39)
    1. 移动Stat
    2. 创建StatGroup
    
    Close #34

--- Assets/src/Entity/EntityStat.cs
+++ Assets/src/Entity/EntityStat.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
 public class EntityStat: MonoBehaviour
 {
-    [SerializeField] private Stat vitality;
     [SerializeField] private Stat maxHealth;
+    [SerializeField] private StatMajorGroup major;
+    [SerializeField] private StatOffenceGroup offence;
+    [SerializeField] private StatDefenceGtoup defence;
 
     public float GetMaxHealth()
     {
         float baseHp = maxHealth.GetValue();
-        float bonusHp = vitality.GetValue() * 5;
+        float bonusHp = major.vitality.GetValue() * 5;
         return baseHp + bonusHp;
     }
 }
---
+++ Assets/src/StatSystem/StatOffenceGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+
+[Serializable]
+public class StatOffenceGroup
+{
+    // Physical Damage
+    public Stat damage; // 伤害
+    public Stat critPower; // 暴击伤害
+    
+    // Elemental Damage
+    public Stat critChange; // 暴击概率
+    public Stat fireDamage; // 火焰伤害
+    public Stat iceDamage; // 冰冻伤害
+    public Stat lightingDamage; // 光明伤害
+}
